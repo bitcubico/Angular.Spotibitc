2ca58d9f84161d20f5109e7ee275e54b593c183f
BITCUBICO Technology <bitcubico@gmail.com>
2020-12-12 10:59:14 -0500

    Adecuo el pipe domseguro a la aplicación

--- src/app/pipes/domseguro.pipe.ts
+++ src/app/pipes/domseguro.pipe.ts
@@ -7,9 +7,10 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class DomseguroPipe implements PipeTransform {
 
   constructor(private _domSanitizer: DomSanitizer){}
-  transform(value: string, url: string): any {
+  transform(value: string): any {
+    const uri = 'https://open.spotify.com/embed?uri=';
     // Verificamos que la url no contenga información maliciosa y transformamos la información
-    return this._domSanitizer.bypassSecurityTrustResourceUrl(url + value);
+    return this._domSanitizer.bypassSecurityTrustResourceUrl(uri + value);
   }
 
 }
